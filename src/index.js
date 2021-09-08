b86635a4e1bc3bc242cb9ebdde02cd13b22a1a55
Rico Perez <ricoperez1@gmail.com>
2021-09-07 20:39:33 -0500

    src file

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import './components/assets/css/style.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, fas);
+
+ReactDOM.render(<App />, document.getElementById("root"));
